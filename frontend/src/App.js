dd338bf8d040d81834a96a3d42bcb51a5472cccf
jeferson <jeferson.santos@ilog.com.br>
2020-08-22 18:41:40 -0300

    Components React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 import Header from "./Components/Header";
 
 function App(){
-    return <Header />;
+    return (
+        <>
+            <Header title="HomePage" >
+                <ul>
+                    <li>HomePage</li>
+                    <li>Projects</li>
+                </ul>
+            </ Header>
+            <Header title="HomePage2">
+                <ul>
+                    <li>HomePage2</li>
+                    <li>Projects2</li>
+                    <li>Login</li>
+                </ul>
+            </Header>
+        </>
+        );
 }
 
 export default App;
