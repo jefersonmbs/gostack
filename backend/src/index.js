9ac68422f312a63fcc8c02232e0a219965d5e156
jeferson <jeferson.santos@ilog.com.br>
2020-08-23 18:39:08 -0300

    Refactory back-end

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,13 @@ const app = express();
 app.use(express.json())
 
 const projects = [];
-app.get("/projects" ,(req, res) => {
+
+app.get("/projects", (req, res) => {
     return res.json(projects);
 })
-app.post("/projects",(req, res) => {
-    const {title, owner } = req.body;
+
+app.post("/projects", (req, res) => {
+    const {title, owner} = req.body;
     const project = {
         id: uuid(),
         title,
@@ -19,9 +21,25 @@ app.post("/projects",(req, res) => {
     projects.push(project);
     res.json(project);
 })
+app.put('/projects/:id', ((req, res) => {
+    const { id } = req.params;
+    return res.json([
+        "projeto 4",
+        "projeto 2",
+        "projeto 3",
+    ])
+
+}))
+app.delete('/projects/:id', ((req, res) => {
+        const { id } = req.params;
+        return res.json([
+            "projeto 4",
+            "projeto 2",
+        ])
+    })
+)
 
 
-
-app.listen(3339, () =>{
+app.listen(3339, () => {
     console.log("Server on 🤖")
 })
