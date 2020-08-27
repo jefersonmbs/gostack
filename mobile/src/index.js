4011d49ea297fd7bf1012e7be3a2d9727c7b161b
jeferson <jeferson.santos@ilog.com.br>
2020-08-26 22:22:37 -0300

    Mobile

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import { View , Text, StyleSheet , StatusBar } from 'react-native';
+import api from './services/api'
 
 //View: Div, footer, header, main, aside , section
 //Text: p, span, strong, h1,h2,h3
 
 export default function App(){
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('/projects').then(response =>  {
+            setProjects(response.data);
+        })
+    }, [])
+
+
     return (
         <>
             <StatusBar barStyle="light-content" backgroundColor="#711797" transparent/>
             <View style={styles.container}>
-                <Text style={styles.title}>Hello Word</Text>
+                {projects.map(project =><Text style={styles.title} key={project.id}>{project.title}</Text>)}
             </View>
         </>
     )
